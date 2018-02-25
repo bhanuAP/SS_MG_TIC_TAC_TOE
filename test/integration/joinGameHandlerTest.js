0f69e20e51ee4bd321a857c17458628cc8788ecc
Bhanu Teja <bhanuteja5481@gmail.com>
2018-02-25 19:33:57 +0530

    Added tests for joinGameHandlers.js

---
+++ test/integration/joinGameHandlerTest.js
@@ -0,0 +1,34 @@
+const assert = require('chai').assert;
+const request = require('supertest');
+const app = require('../../app.js');
+const gameIdGen = app.uniqueNumberGernerator;
+let games=app.games;
+
+describe("# App",()=>{
+  beforeEach(done => {
+    app.uniqueNumberGernerator = () => {
+      return 1234;
+    };
+    request(app)
+    .post("/game/joinGameCreator")
+    .send("gameCreator=Bhanu")
+    .end(done);
+  });
+  before(()=>{
+    app.games = {};
+  })
+
+  after(() => {
+    app.uniqueNumberGernerator = gameIdGen;
+    app.games = games;
+  });
+
+  describe("## /game/TICTACTOE1234/hasPlayerJoined",()=>{
+    it("should get true if all players are joined",done=>{
+      request(app)
+      .get('/game/TICTACTOE1234/hasPlayerJoined')
+      .expect('{"start":false,"link":"/game/TICTACTOE1234"}')
+      .end(done);
+    });
+  });
+});
